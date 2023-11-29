da6f205b09ec54ebb24adc47bd048fdbc5680927
salaheddinesamid <salaheddine.kobra@iu-study.org>
2023-11-29 13:54:47 +0100

    new feature

---
+++ training.js
@@ -0,0 +1,18 @@
+//Variables:
+//constant:
+const x = 1
+//variable:
+let y = 9
+var z = 2
+//------------------------Data Types--------------------------//
+let name = "Name"
+let arr = ["N","a","m","e"]
+//console.log(arr[0])
+//------------------------Functions--------------------------//
+//Defining function:
+function product(x,y,z){
+    let result = x*y*z
+    console.log(result); 
+}
+//Executing function:
+product(2,3,4)
